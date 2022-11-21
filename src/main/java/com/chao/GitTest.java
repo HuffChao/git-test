6d160d5b279b2b0c723e34be2ab692854221be6c
chaoGieGie <1979407221@qq.com>
2022-11-21 03:21:23 +0000

    update src/main/java/com/chao/GitTest.java.
    Signed-off-by: chaoGieGie <1979407221@qq.com>

--- src/main/java/com/chao/GitTest.java
+++ src/main/java/com/chao/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hot-fix2");
         System.out.println("push");
         System.out.println("hello gitee");
+        System.out.println("gitee pull");
     }
 
 }
