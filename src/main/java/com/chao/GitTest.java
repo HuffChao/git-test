3ac8e7e46768d281faae951eab0d3852ae9b1011
HuffChao <118613345+HuffChao@users.noreply.github.com>
2022-11-21 09:43:18 +0800

    Update GitTest.java

--- src/main/java/com/chao/GitTest.java
+++ src/main/java/com/chao/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git4 hot-fix");
         System.out.println("hello git 5");
         System.out.println("hot-fix2");
+        System.out.println("push");
     }
 
 }
