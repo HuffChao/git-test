495afc56f582c5c02d2e7701877e3b2c2d1e53a7
HuffChao <HuffChao@chaogiegie.com>
2022-11-21 11:20:18 +0800

    gitee commit1

--- src/main/java/com/chao/GitTest.java
+++ src/main/java/com/chao/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello git 5");
         System.out.println("hot-fix2");
         System.out.println("push");
+        System.out.println("hello gitee");
     }
 
 }
