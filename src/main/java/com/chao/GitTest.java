0cec106192d3dc9b12f5a2bdadb6bf52a12b0770
Administrator <admin@example.com>
2022-11-22 20:59:42 +0000

    gitlab commit1

--- src/main/java/com/chao/GitTest.java
+++ src/main/java/com/chao/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("push");
         System.out.println("hello gitee");
         System.out.println("gitee pull");
+        System.out.println("hello gitlab")
     }
 
 }
